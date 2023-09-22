dc181811a839e3e70873841bbcc2ef80ac476869
David Acosta <davidacosta@Davids-Mac-mini.attlocal.net>
2023-09-22 18:16:26 -0400

    first commit

---
+++ SwiftfulFirebaseBootcamp/SwiftfulFirebaseBootcampApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftfulFirebaseBootcampApp.swift
+//  SwiftfulFirebaseBootcamp
+//
+//  Created by David Acosta on 9/22/23.
+//
+
+import SwiftUI
+
+@main
+struct SwiftfulFirebaseBootcampApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
